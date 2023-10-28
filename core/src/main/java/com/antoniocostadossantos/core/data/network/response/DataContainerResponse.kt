2c8ca5e9d703322c1e0df4f2209f3c82d7e9586f
Antonio <antonio195br@gmail.com>
2023-10-28 13:53:04 -0300

    Mapeando os objetos da API

---
+++ core/src/main/java/com/antoniocostadossantos/core/data/network/response/DataContainerResponse.kt
@@ -0,0 +1,5 @@
+package com.antoniocostadossantos.core.data.network.response
+
+data class DataContainerResponse(
+    val results: List<CharacterResponse>
+)
---
+++ core/src/main/java/com/antoniocostadossantos/core/data/network/response/DataWrapperResponse.kt
@@ -0,0 +1,6 @@
+package com.antoniocostadossantos.core.data.network.response
+
+data class DataWrapperResponse(
+    val copyright: String,
+    val data: DataContainerResponse
+)
