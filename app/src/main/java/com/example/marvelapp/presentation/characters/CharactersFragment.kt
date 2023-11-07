8ff8423612afc4fc1581819e9d9214505659e48b
Antonio <antonio195br@gmail.com>
2023-11-07 13:17:55 -0300

    Melhorando a organizacao do projeto

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,16 +5,87 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.marvelapp.R
+import com.antoniocostadossantos.core.domain.model.Character
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
+    private var _binding: FragmentCharactersBinding? = null
+    private val binding: FragmentCharactersBinding get() = _binding!!
+
+    private val charactersAdapter = CharactersAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    ) = FragmentCharactersBinding.inflate(layoutInflater).apply { _binding = this }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+
+        charactersAdapter.submitList(
+            listOf(
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+                Character(
+                    "Spider Man",
+                    "https://tm.ibxk.com.br/2023/10/15/15144301590000.jpg?ims=352x208"
+                ),
+
+                )
+        )
+
     }
 
+    private fun initCharactersAdapter() {
+        with(binding.recyclerViewCharacters) {
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
+    }
 }
---
+++ core/src/main/java/com/antoniocostadossantos/core/domain/model/Character.kt
@@ -0,0 +1,6 @@
+package com.antoniocostadossantos.core.domain.model
+
+data class Character(
+    val name: String,
+    val image: String
+)
