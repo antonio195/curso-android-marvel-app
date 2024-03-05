b70183010be90c103a5c4b013bbdf1023ceaf78b
Antonio <antonio195br@gmail.com>
2024-03-05 08:19:46 -0300

    Continuar de onde parei

--- app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
+++ app/src/main/java/com/example/marvelapp/framework/network/response/CharacterResponse.kt
@@ -1,7 +1,16 @@
 package com.example.marvelapp.framework.network.response
 
+import com.antoniocostadossantos.core.domain.model.Character
+
 data class CharacterResponse(
     val id: String,
     val name: String,
     val thumbnailResponse: ThumbnailResponse
 )
+
+fun CharacterResponse.toCharacterModel(): Character {
+    return Character(
+        name = this.name,
+        image = "${this.thumbnailResponse.path}.${this.thumbnailResponse.extension}"
+    )
+}
